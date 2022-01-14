5eec3ec60b784f795de4b936c0c4d629d47a9537
shufuya <1357895146@qq.com>
2022-01-14 23:44:30 +0800

    first

---
+++ src/view/Father.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Son from "./Son";
+
+export default function Father() {
+  return (
+    <div>
+      <Son></Son>
+    </div>
+  );
+}
---
+++ src/view/Son.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Son() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
